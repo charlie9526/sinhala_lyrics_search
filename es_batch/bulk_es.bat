1840d270ff28638752ed92ce366f7c167618b126
charlie9526 <chanaka.16@cse.mrt.ac.lk>
2020-07-03 22:05:00 +0530

    Signed-off-by: charlie9526 <chanaka.16@cse.mrt.ac.lk>

--- es_batch/bulk_es.bat
+++ es_batch/bulk_es.bat
@@ -1,7 +1,7 @@
 set ES_HOST_POST=localhost:9200
 
 cd ..\json_converter
-curl -H "Content-Type: application/json" -XPOST "%ES_HOST%/test_lyrics/_bulk?pretty&refresh" --data-binary "@bulk_sin_lyrics.json"
+curl -H "Content-Type: application/json" -XPOST "%ES_HOST_POST%/test_lyrics/_bulk?pretty&refresh" --data-binary "@bulk_sin_lyrics.json"
 curl "localhost:9200/_cat/indices?v"
 
 cd /d E:\projects\7.ES\charlie_sinhala_lyrics\es_batch
